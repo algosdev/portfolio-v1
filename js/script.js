216bb1201d04f93bf8cefa433d48316913bd4a6f
prince-algo <samandarofficial149@gmail.com>
2020-07-29 17:17:31 +0500

    Fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,34 +1,52 @@
 const bar = document.querySelector('.bar');
-const projectNav = document.querySelectorAll('.project-nav p');
 const web = document.querySelector('#web');
 const app = document.querySelector('#app');
 const appPro = document.querySelectorAll('#pro')[0];
 const webPro = document.querySelectorAll('#pro')[1];
+const box = document.querySelectorAll('.more .btn');
+const close = document.querySelector('.modal .close');
+const modal = document.querySelector('.modal');
+
+close.addEventListener('click', () => {
+    modal.classList.add('hide');
+    document.querySelector('#container').style.filter = 'blur(0)'
+})
+box.forEach(el => {
+    el.addEventListener('click', () => {
+        modal.classList.remove('hide');
+        document.querySelector('#container').style.filter = 'blur(10px)'
+    })
+})
 
 web.addEventListener('click', () => {
-    changePro(appPro, webPro, app, web);
+    changePro(webPro, appPro, app, web);
 });
 app.addEventListener('click', () => {
-    changePro(webPro, appPro, web, app);
+    changePro(appPro, webPro, web, app);
 });
 function changePro(a, b, c, d) {
     anime({
         targets: a,
         scale: [1, 0],
+        opacity: [1, 0],
         easing: 'easeInOutQuad',
-        duration: 500
+        duration: 200
     });
     setTimeout(() => {
         a.style.display = 'none';
-    }, 500);
-    b.style.transform = 'scale(0)';
-    b.style.display = 'grid';
-    anime({
-        targets: b,
-        scale: [0, 1],
-        easing: 'easeInOutQuad',
-        duration: 500
-    });
+    }, 200);
+    setTimeout(() => {
+        b.style.transform = 'scale(0)';
+        b.style.opacity = '0';
+        b.style.display = 'grid';
+        anime({
+            targets: b,
+            scale: [0, 1],
+            opacity: [0, 1],
+            easing: 'easeInOutQuad',
+            duration: 300
+        });
+    }, 200)
     c.className = '';
     d.className = 'active';
 }
